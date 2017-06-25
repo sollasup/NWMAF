a00d59d9c1d36a1d04f2e468ed27ea2af0061c2f
Sebastian Soller <sebastian.soller93@gmail.com>
2017-06-25 21:14:13 +0200

    Framework settings

--- LoadRawData.py
+++ LoadRawData.py
@@ -2,8 +2,11 @@ import LoadRawData.py
 import numpy as np
 
 #Famework Settings
+#--------Data Setup-------------
+#Check one if the framework was used once and the Feature file thereby have already been created
+Featurefilesaved = 0
 
-#Select Data Set
+#--------Select Data Set------------
 #Xsens setup
 UseXsensData = 1
 if(UseXsensData==1):
@@ -12,15 +15,17 @@ if(UseXsensData==1):
     Session3 = 1
     Session4 = 1
     Session5 = 1
+    Unlabeleddata = 1
 
-USeShimmerData = 1
+#Shimmer setup
+UseShimmerData = 1
+if(UseShimmerData==1):
+    Unlabeleddata = 1
 
-#Select Data Segmentation
+#---------Select Data Segmentation-----------
 #What amount of data is used to calculate features for each classification
 #Evaluate for every camera walktrought- for the Xsens data around 3-5, for the Shimmer data around 20 strides
 CameraSegment = 1
 StrideSegment = 1
 
-
-
-#Load the collected data from recordings
+#--------Sensor Selection-------------
